86eb2aa0c16c42d8720f913ccaaf875b80697b82
Adrien Joly <adrien@whyd.com>
2017-02-08 16:35:08 +0100

    load tracks from openwhyd, using jsonp

--- openwhyd-search.js
+++ openwhyd-search.js
@@ -1,47 +1,58 @@
-var profileUrl = 'profile.json' // 'https://openwhyd.org/adrien?format=json'
+var username = 'adrien'
+var callbackFctName = 'onOpenwhydData'
+var profileUrl = 'https://openwhyd.org/' + username +'?limit=99999&callback=' + callbackFctName
 
-var searchBox = document.getElementById('search-box')
-var searchResults = document.getElementById('search-results')
+// DATA MODEL
 
 var trackIndex = {}
 
-fetch(profileUrl, { mode: 'no-cors' })
-  .then(function(response) {
-    return response.json()
-  })
-  .then(function(tracks) {
-    //console.log('json =>', tracks.map((track) => track.name))
-    tracks.forEach((track) => {
-      var words = track.name.toLowerCase().split(' ')
-      words.forEach((word) => {
-        trackIndex[word] = (trackIndex[word] || []).concat([ track ])
-      })
-    })
-    console.log('index:', trackIndex)
-  })
+function search(term) {
+  console.log('query:', term)
+  var results = trackIndex[term]
+  return results || []
+}
 
-searchBox.onchange = function() {
-  console.log('query:', searchBox.value)
-  var results = trackIndex[searchBox.value]
-  results.forEach((result) => {
-    var text = document.createTextNode('query: ' + searchBox.value
-    + ' => ' + result.name)
-    
+function indexTracks(tracks) {
+  console.log('loaded', tracks.length, 'tracks')
+  tracks.forEach((track) => {
+    var words = track.name.toLowerCase().split(' ')
+    words.forEach((word) => {
+      trackIndex[word] = (trackIndex[word] || []).concat([ track ])
+    })
+  })
+}
+
+// LOAD PROFILE DATA FROM OPENWHYD
+
+window[callbackFctName] = indexTracks
+var script = document.createElement('script')
+script.src = profileUrl
+document.body.appendChild(script)
+
+// UI INIT
+
+var ui = (function(document, search){
+
+  var searchBox = document.getElementById('search-box')
+  var searchResults = document.getElementById('search-results')
+
+  function clearResults() {
+    searchResults.innerHTML = ''
+  }
+
+  function appendResult(result) {
     var a = document.createElement('a')
     a.href = 'https://openwhyd.org/c/' + result._id
-    a.appendChild(text)
-
+    a.appendChild(document.createTextNode(result.name))
     var li = document.createElement('li')
     li.appendChild(a)
-    
     searchResults.appendChild(li)
-  })
-}
+  }
 
-/*
-var script = document.getElementById('profile-data');
-script.onload = function() {
-  console.log('loaded', script.innerHTML)
-}
-script.src = profileUrl
-*/
+  searchBox.onchange = function() {
+    clearResults()
+    var results = search(searchBox.value)
+    results.forEach(appendResult)
+  }
+
+})(document, search)
--- populate.sh
+++
@@ -1,1 +0,0 @@
-curl "https://openwhyd.org/adrien?format=json&limit=99999" >profile.json
